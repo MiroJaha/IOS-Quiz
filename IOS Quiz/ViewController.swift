fc044fdcdb58a1d82e8586db5221450bf8eab6f5
Mohammad Jaha <miro.tune@gmail.com>
2021-12-27 20:23:51 +0000

    Add Animated Background

--- IOS Quiz/ViewController.swift
+++ IOS Quiz/ViewController.swift
@@ -6,11 +6,15 @@
 //
 
 import UIKit
+import SpriteKit
 
 class ViewController: UIViewController {
     
     @IBOutlet weak var questionLabel: UILabel!
     @IBOutlet weak var AnswerLabel: UILabel!
+    
+    let starsView = SKView()
+    let rainView = SKView()
     
     let quizBank = [
             ("What is the first step of iOS development?", "Create the user interface. As we create the UI, we will get a better understanding of our application. Then we breath life into our application."),
@@ -24,7 +28,9 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        addDoubleAndTripleGestureRecognizer()
+        createStarLayers()
+        createRainLayers()
         updateUI()
     }
 
@@ -44,6 +50,118 @@ class ViewController: UIViewController {
         questionLabel.text = quizBank[currentQuestion].0
         AnswerLabel.text = quizBank[currentQuestion].1
         AnswerLabel.isHidden = true
+    }
+    
+    //Creates a new star field
+    func starfieldEmitterNode(text: String, speed: CGFloat, lifetime: CGFloat, scale: CGFloat, birthRate: CGFloat, color: SKColor) -> SKEmitterNode {
+        let star = SKLabelNode(fontNamed: "Helvetica")
+        star.fontSize = 80.0
+        star.text = text
+        let textureView = SKView()
+        let texture = textureView.texture(from: star)
+        texture!.filteringMode = .nearest
+
+        let emitterNode = SKEmitterNode()
+        emitterNode.particleTexture = texture
+        emitterNode.particleBirthRate = birthRate
+        emitterNode.particleColor = color
+        emitterNode.particleLifetime = lifetime
+        emitterNode.particleSpeed = speed
+        emitterNode.particleScale = scale
+        emitterNode.particleColorBlendFactor = 1
+        emitterNode.position = CGPoint(x: view.frame.midX, y: view.frame.maxY)
+        emitterNode.particlePositionRange = CGVector(dx: view.frame.maxX, dy: 0)
+        emitterNode.particleSpeedRange = 16.0
+
+        //Rotates the stars
+        emitterNode.particleAction = SKAction.repeatForever(SKAction.sequence([
+            SKAction.rotate(byAngle: CGFloat(-.pi / 4.0), duration: 1),
+            SKAction.rotate(byAngle: CGFloat(Double.pi / 4), duration: 1)]))
+
+        //Causes the stars to twinkle
+        let twinkles = 20
+        let colorSequence = SKKeyframeSequence(capacity: twinkles*2)
+        let twinkleTime = 1.0 / CGFloat(twinkles)
+        for i in 0..<twinkles {
+            colorSequence.addKeyframeValue(SKColor.white,time: CGFloat(i) * 2 * twinkleTime / 2)
+            colorSequence.addKeyframeValue(SKColor.yellow, time: (CGFloat(i) * 2 + 1) * twinkleTime / 2)
+        }
+        emitterNode.particleColorSequence = colorSequence
+
+        emitterNode.advanceSimulationTime(TimeInterval(lifetime))
+        return emitterNode
+    }
+    
+    func createStarLayers() {
+        let u = SKScene()
+        
+        //A layer of a star field
+        let starfieldNode = SKNode()
+        starfieldNode.name = "starfieldNode"
+        starfieldNode.addChild(starfieldEmitterNode(text: "✦", speed: -48, lifetime: self.view.frame.height / 23, scale: 0.2, birthRate: 1, color: SKColor.lightGray))
+        u.addChild(starfieldNode)
+        u.size = CGSize(width: view.frame.width, height:  view.frame.height)
+        
+        starsView.presentScene(u)
+        starsView.frame = .init(x: 0, y: 0, width: view.frame.width, height: view.frame.height)
+        view.addSubview(starsView)
+        view.sendSubviewToBack(starsView)
+        //addChild(starfieldNode)
+
+        //A second layer of stars
+        var emitterNode = starfieldEmitterNode(text: "✦", speed: -40, lifetime: self.view.frame.height / 10, scale: 0.14, birthRate: 2, color: SKColor.gray)
+        emitterNode.zPosition = -10
+        starfieldNode.addChild(emitterNode)
+
+        //A third layer
+        emitterNode = starfieldEmitterNode(text: "✦", speed: -20, lifetime: self.view.frame.height / 5, scale: 0.1, birthRate: 5, color: SKColor.darkGray)
+        starfieldNode.addChild(emitterNode)
+    }
+    
+    func createRainLayers() {
+        print("Double Clicked")
+        let u = SKScene()
+        
+        //A layer of a star field
+        let starfieldNode = SKNode()
+        starfieldNode.name = "starfieldNode"
+        starfieldNode.addChild(starfieldEmitterNode(text: "|", speed: -148, lifetime: self.view.frame.height / 23, scale: 0.2, birthRate: 4, color: SKColor.lightGray))
+        u.addChild(starfieldNode)
+        u.size = CGSize(width: view.frame.width, height:  view.frame.height)
+        
+        rainView.presentScene(u)
+        rainView.frame = .init(x: 0, y: 0, width: view.frame.width, height: view.frame.height)
+        view.addSubview(rainView)
+        view.sendSubviewToBack(rainView)
+        //addChild(starfieldNode)
+
+        //A second layer of stars
+        var emitterNode = starfieldEmitterNode(text: "|", speed: -300, lifetime: self.view.frame.height / 10, scale: 0.14, birthRate: 8, color: SKColor.gray)
+        emitterNode.zPosition = -10
+        starfieldNode.addChild(emitterNode)
+
+        //A third layer
+        emitterNode = starfieldEmitterNode(text: "|", speed: -70, lifetime: self.view.frame.height / 5, scale: 0.1, birthRate: 20, color: SKColor.darkGray)
+        starfieldNode.addChild(emitterNode)
+    }
+    
+    @objc func starsViewLoad() {
+        starsView.isHidden = false
+        rainView.isHidden = true
+    }
+    
+    @objc func rainViewLoad() {
+        rainView.isHidden = false
+        starsView.isHidden = true
+    }
+    
+    func addDoubleAndTripleGestureRecognizer(){
+        let doubleTapRecognizer = UITapGestureRecognizer(target: self, action: #selector(starsViewLoad))
+        doubleTapRecognizer.numberOfTapsRequired = 2
+        view.addGestureRecognizer(doubleTapRecognizer)
+        let tripleTapRecognizer = UITapGestureRecognizer(target: self, action: #selector(rainViewLoad))
+        tripleTapRecognizer.numberOfTapsRequired = 3
+        view.addGestureRecognizer(tripleTapRecognizer)
     }
 }
 
